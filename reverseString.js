97ddf74d91cb87a06f87654ee2e63ec08e05c16f
Bailey Day <bailey.j.day@gmail.com>
2020-08-11 15:41:03 -0400

    reverse a string

---
+++ reverseString.js
@@ -0,0 +1,16 @@
+string = "cat";
+
+const reverseString = function (str) {
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "No String";
+  }
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+  console.log(backwards);
+  return backwards.join("");
+};
+
+reverseString(string);
