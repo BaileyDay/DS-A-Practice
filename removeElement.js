263c11aa354552da5025d39dd26f9fe97fdb5846
Bailey Day <bailey.j.day@gmail.com>
2020-08-11 18:47:34 -0400

    Algos

---
+++ removeElement.js
@@ -0,0 +1,11 @@
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+var removeElement = function (nums, val) {
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] == val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
